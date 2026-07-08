2675a2e259c2a6f42a2dddd3a41cf523a0833674
vinoth <mullaivinoth.gmail.com>
2026-07-08 18:01:58 +0530

    import-snet--demo1

--- environments/lab/imports.tf
+++ environments/lab/imports.tf
@@ -1,5 +1,5 @@
 
 import {
-  to = azurerm_subnet.genconfig_test
-  id = "/subscriptions/d6e34000-17b3-4ff6-b09d-47bee0f62b26/resourceGroups/vwan-lab-rg/providers/Microsoft.Network/virtualNetworks/vnet-bastion-lab/subnets/snet-genconfig-test"
+  to = azurerm_subnet.demo1_import
+  id = "/subscriptions/d6e34000-17b3-4ff6-b09d-47bee0f62b26/resourceGroups/vwan-lab-rg/providers/Microsoft.Network/virtualNetworks/vnet-bastion-lab/subnets/snet-demo-import"
 }
