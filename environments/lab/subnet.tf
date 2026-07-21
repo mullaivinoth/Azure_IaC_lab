c418afddec1dffeff5ea0ab52a732ba1e606bca9
vinoth <mullaivinoth@gmail.com>
2026-07-21 17:00:55 +0530

    creating-snet-my-d11

--- environments/lab/subnet.tf
+++ environments/lab/subnet.tf
@@ -5,3 +5,9 @@ resource "azurerm_subnet" "db" {
   address_prefixes                = ["10.101.2.0/26"]
   default_outbound_access_enabled = false
 }
+resource "azurerm_subnet" "snet-my-d11" {
+  name                 = "snet-my-d11-name"
+  resource_group_name  = "vwan-lab-rg"
+  virtual_network_name = "vnet-bastion-lab"
+  address_prefixes     = ["10.101.11.0/26"]
+}
