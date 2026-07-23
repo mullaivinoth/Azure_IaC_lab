99234af55de82a93e29e7599398fbd7ef7ff73a7
mullaivinoth <mullaivinoth@gmail.com>
2026-07-23 16:43:07 +0530

    Revert "snet-db-demo11-23-jul-26"

--- environments/lab/subnet.tf
+++ environments/lab/subnet.tf
@@ -5,10 +5,3 @@ resource "azurerm_subnet" "db" {
   address_prefixes                = ["10.101.2.0/26"]
   default_outbound_access_enabled = false
 }
-resource "azurerm_subnet" "db-demo11" {
-  name                            = "snet-db-demo11"
-  resource_group_name             = azurerm_resource_group.lab.name
-  virtual_network_name            = azurerm_virtual_network.bastion.name
-  address_prefixes                = ["10.101.11.0/26"]
-  default_outbound_access_enabled = false
-}
