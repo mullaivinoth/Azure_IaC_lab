b059115171410c9cc177bb94e77bca6c35fa4b24
vinoth <mullaivinoth@gmail.com>
2026-07-24 17:08:59 +0530

    create demo snet app11

--- environments/lab/subnet.tf
+++ environments/lab/subnet.tf
@@ -5,3 +5,10 @@ resource "azurerm_subnet" "db" {
   address_prefixes                = ["10.101.2.0/26"]
   default_outbound_access_enabled = false
 }
+resource "azurerm_subnet" "demo-app11" {
+  name                            = "snet-demo-app11"
+  resource_group_name             = azurerm_resource_group.lab.name
+  virtual_network_name            = azurerm_virtual_network.bastion.name
+  address_prefixes                = ["10.101.11.0/26"]
+  default_outbound_access_enabled = false
+}
